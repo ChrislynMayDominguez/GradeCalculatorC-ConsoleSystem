868512a8a2f26b940d756096a9ed440d3c7f953b
ChrislynMayDominguez <chrislyn.dominguez@jmc.edu.ph>
2025-02-11 15:57:10 +0800

    first commit

--- obj/Debug/net9.0/GradeCalculator.AssemblyInfo.cs
+++ obj/Debug/net9.0/GradeCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GradeCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c82f73dcffac97ea0675c89bf3b00db6f337fb01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+850297d712b4cc60739f7a938b64ed2cbfcdfdb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("GradeCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GradeCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
